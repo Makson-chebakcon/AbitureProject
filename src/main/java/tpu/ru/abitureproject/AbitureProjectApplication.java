8905c72190bf18883eede5b6d43b3241aabb4f4e
Makson-chebakcon <makc15655@gmail.com>
2023-09-06 13:38:29 +0700

    first commit

---
+++ src/main/java/tpu/ru/abitureproject/AbitureProjectApplication.java
@@ -0,0 +1,13 @@
+package tpu.ru.abitureproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbitureProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbitureProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tpu/ru/abitureproject/AbitureProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package tpu.ru.abitureproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbitureProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
